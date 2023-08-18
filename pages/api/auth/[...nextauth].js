f6ecffd351c16ab01fb88b02accc22752e7d5b93
M ZAIN ALI <107149449+mzain42401@users.noreply.github.com>
2023-08-18 18:42:22 +0500

    first

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,25 +3,25 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { checkData, checkDataPassword } from "../services/credential";
 
 export const authOptions = {
-    session:{
-        jwt:true
-    },
-  // Configure one or more authentication providers
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    jwt: true
+  },
   providers: [
     CredentialsProvider({
-        async authorize({email,password}) {
-            const isEmail=checkData(email)
-            if(!isEmail){
-throw new Error("user not found")
-            }
-            const isvalid =await checkDataPassword(isEmail.password,password) 
-            if (!isvalid) {
-throw new Error("incorrect password")
-                
-            }
-            return{email}
+      async authorize({ email, password }) {
+        const isEmail = checkData(email)
+        if (!isEmail) {
+          throw new Error("user not found")
+        }
+        const isvalid = await checkDataPassword(isEmail.password, password)
+        if (!isvalid) {
+          throw new Error("incorrect password")
 
         }
+        return { email }
+
+      }
 
     })
   ],
